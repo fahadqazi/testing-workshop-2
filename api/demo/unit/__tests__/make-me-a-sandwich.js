07fd518014c617210f6b9db150fdc7a2b91d0ea3
Fahad Qazi <fahadqazi@gmail.com>
2017-07-06 23:06:09 +0100

    first test

--- api/demo/unit/__tests__/make-me-a-sandwich.js
+++ api/demo/unit/__tests__/make-me-a-sandwich.js
@@ -1,1 +1,7 @@
-test('todo', () => {})
+import makeMeASandwich from '../make-me-a-sandwich'
+
+test('should return null if sandwich dont exist', () => {
+    const req = {query: ''}
+    const result = makeMeASandwich(req)
+    expect(result).toBeNull()
+})
