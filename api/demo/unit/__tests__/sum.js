f7cab7b6a2f7cbb6d023b6a218d4a3be1b24e377
Fahad Qazi <fahadqazi@gmail.com>
2017-07-07 21:33:48 +0100

    adding a test case, using tdd

--- api/demo/unit/__tests__/sum.js
+++ api/demo/unit/__tests__/sum.js
@@ -1,2 +1,9 @@
 // Let's test drive summing numbers
-test('todo!', () => {})
+// sum() -> 0
+// sum(5) -> 5
+// sum(4, 5, 6) -> 15
+
+test('sum(0) should return 0', () => {
+    const result = sum()
+    expect(sum()).toBe(0);
+})
