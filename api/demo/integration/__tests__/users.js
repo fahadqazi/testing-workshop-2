4d545f21828612d53230d4eaeb738e06e0e1b59d
Fahad Qazi <fahadqazi@gmail.com>
2017-07-09 22:41:47 +0100

    adding test case for server setup

--- api/demo/integration/__tests__/users.js
+++ api/demo/integration/__tests__/users.js
@@ -1,1 +1,22 @@
-test('todo', () => {})
+import startServer from '../start-server';
+import axios from 'axios';
+
+let server;
+
+beforeAll(() => {
+  return startServer().then(s => {
+    server = s
+  })
+})
+
+afterAll(done => {
+  server.close(done);
+})
+
+test('can get users', () => {
+    return axios
+        .get('http://localhost:3001/api/')
+        .then(response => {
+            console.log(response.data.users)
+        })
+})
